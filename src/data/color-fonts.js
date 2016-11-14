da655cf822fe7edaeb253692f501e1f8b6f4705a
Bram Stein <b.l.stein@gmail.com>
2016-11-14 09:59:07 +0100

    Update color font support.

--- src/data/color-fonts.js
+++ src/data/color-fonts.js
@@ -1,31 +1,33 @@
 define(function () {
   return {
     name: "Color fonts",
-    description: "Four color font formats are currently in existence: COLR/CPAL, SBIX, OpenType-SVG, and CBDT/CBLC. Support is fragmented. When unsupported, the browser will fall back to regular glyphs in the font. Read about [the differences of the formats](http://pixelambacht.nl/2014/multicolor-fonts/).",
+    description: "Four color font formats are currently in existence: COLR/CPAL, sbix, OpenType-SVG, and CBDT/CBLC. Support is fragmented. When unsupported, the browser will fall back to regular glyphs in the font. Read about [the differences of the formats](http://pixelambacht.nl/2014/multicolor-fonts/).",
     keywords: ["color", "multicolor"],
     browsers: [
       { name: "ie",      range: "-10", support: "no" },
-      { name: "ie",      range: "11-",  support: "yes", note: "Only COLR/CPAL on Windows 8.1+." },
-      { name: "edge",    range: "-", support: "no" },
+      { name: "ie",      range: "11-",  support: "partial", note: "Only COLR/CPAL on Windows 8.1+." },
+      { name: "edge",    range: "-37", support: "partial", note: "Only COLR/CPAL." },
+      { name: "edge",    range: "38-", support: "yes" },
       { name: "chrome",  range: "-",  support: "no" },
       { name: "firefox", range: "-35", support: "no" },
-      { name: "firefox", range: "36-",  support: "yes", note: "Only COLR/CPAL and OpenType-SVG." },
+      { name: "firefox", range: "36-",  support: "partial", note: "Only COLR/CPAL and OpenType-SVG." },
       { name: "opera",   range: "-",  support: "no" },
       { name: "safari",  range: "-8", support: "no" },
-      { name: "safari",  range: "9-", support: "yes", note: "Only SBIX." },
+      { name: "safari",  range: "9-", support: "partial", note: "Only sbix." },
       { name: "ios",     range: "-8.1", support: "no" },
-      { name: "ios",     range: "9.1-", support: "yes", note: "Only Apple's proprietary SBIX." },
+      { name: "ios",     range: "9.1-", support: "partial", note: "Only sbix." },
       { name: "android", range: "-",  support: "no" }
     ],
     features: [
       {
-        name: "SBIX",
-        description: "Apple's implementation of color fonts. Uses embedded PNG images to represent the color glyphs.",
+        name: "sbix",
+        description: "Apple's color format. Uses embedded JPG, PNG, or TIFF images to represent the color glyphs.",
         specification: "https://www.microsoft.com/typography/otspec/sbix.htm",
         keywords: ['color', 'multicolor', 'sbix', 'apple'],
         browsers: [
           { name: "ie", range: "-", support: "no" },
-          { name: "edge", range: "-", support: "no" },
+          { name: "edge", range: "-37", support: "no" },
+          { name: "edge", range: "38-", support: "yes" },
           { name: "chrome", range: "-", support: "no" },
           { name: "firefox", range: "-", support: "no" },
           { name: "safari", range: "-8", support: "no" },
@@ -61,7 +63,8 @@ define(function () {
         keywords: ['color', 'multicolor', 'svg', 'adobe', 'mozilla', 'svg-in-opentype', 'opentype-svg'],
         browsers: [
           { name: "ie", range: "-", support: "no" },
-          { name: "edge", range: "-", support: "no" },
+          { name: "edge", range: "-37", support: "no" },
+          { name: "edge", range: "38-", support: "yes" },
           { name: "chrome", range: "-", support: "no" },
           { name: "firefox", range: "-25", support: "no" },
           { name: "firefox", range: "26-", support: "yes" },
@@ -73,18 +76,19 @@ define(function () {
       },
       {
         name: "CBDT/CBLC",
-        description: "Google's CBDT/CBLC font format. Currently only supported on Android when installed as a system font; not available through @font-face. Uses embedded PNG images to represent the color glyphs.",
+        description: "Google's CBDT/CBLC color format. Uses embedded PNG images to represent the color glyphs.",
         specification: "https://www.microsoft.com/typography/otspec/cbdt.htm",
         keywords: ['color', 'multicolor', 'google', 'cbdt', 'cblc'],
         browsers: [
           { name: "ie", range: "-", support: "no" },
-          { name: "edge", range: "-", support: "no" },
+          { name: "edge", range: "-37", support: "no" },
+          { name: "edge", range: "38-", support: "yes" },
           { name: "chrome", range: "-", support: "no" },
           { name: "firefox", range: "-", support: "no" },
           { name: "safari", range: "-", support: "no" },
           { name: "ios", range: "-", support: "no" },
           { name: "opera", range: "-", support: "no" },
-          { name: "android", range: "-", support: "no" }
+          { name: "android", range: "-", support: "no", note: "Only supported on Android when installed as a system font; not available through @font-face." }
         ]
       }
     ]
